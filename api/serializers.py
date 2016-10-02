125387a2a94b43bef402ea2906c0f1b4711f96be
Theofanis Despoudis <thdespou@hotmail.com>
2016-10-02 19:54:22 +0300

    Remove unused folder and objects

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,2 @@
 from rest_framework import serializers
 
-class WeatherInfoSerializer(serializers.Serializer):
-    city = serializers.CharField(max_length=200)
-    min_temp = serializers.FloatField()
-    max_temp = serializers.FloatField()
-    average_temp = serializers.FloatField()
-    humidity = serializers.IntegerField(min_value=0, max_value=100)
